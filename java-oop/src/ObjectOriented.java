dfff9b52715226a2b1077da41278afa4118e1e03
son <son@192.168.100.112>
2016-07-17 11:44:43 +0900

    2016.07.17 	-Hello

--- java-oop/src/ObjectOriented.java
+++ java-oop/src/ObjectOriented.java
@@ -3,7 +3,11 @@ public class ObjectOriented {
 
 	public static void main(String[] args) {
 		System.out.println("Hello GIT World");
-
+			
+		
+		System.out.println("Hello GIT World2");
+		System.out.println("Hello GIT World3");
+		System.out.println("Hello GIT World4");
 	}
 
 }
